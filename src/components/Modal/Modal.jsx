54c08c4d41296fe7eb8cba2bf96c7770052f0193
Glazkov Sergey <glaz.dev@yandex.ru>
2023-07-26 11:02:56 +0300

    feat: modal, portal, OrderDetails

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+import Portal, { createContainer } from "../Portal/Portal";
+
+import Styles from "./Modal.module.css";
+import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+const MODAL_CONTAINER_ID = "modal-container-id";
+
+const Modal = (props) => {
+    const { onClose, children } = props;
+
+    const rootRef = useRef(null);
+    const [isMounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        createContainer({ id: MODAL_CONTAINER_ID });
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        const handleWrapperClick = (event) => {
+            const { target } = event;
+
+            if (target instanceof Node && rootRef.current === target) {
+                onClose?.();
+            }
+        };
+        const handleEscapePress = (event) => {
+            if (event.key === "Escape") {
+                onClose?.();
+            }
+        };
+
+        window.addEventListener("click", handleWrapperClick);
+        window.addEventListener("keydown", handleEscapePress);
+
+        return () => {
+            window.removeEventListener("click", handleWrapperClick);
+            window.removeEventListener("keydown", handleEscapePress);
+        };
+    }, [onClose]);
+
+    const handleClose = useCallback(() => {
+        onClose?.();
+    }, [onClose]);
+
+    return isMounted ? (
+        <Portal id={MODAL_CONTAINER_ID}>
+            <div className={Styles.wrap} ref={rootRef} data-testid="wrap">
+                <div className={`${Styles.content} pr-20 pl-20`}>
+                    <button
+                        type="button"
+                        className={`${Styles.closeButton}`}
+                        onClick={handleClose}
+                        data-testid="modal-close-button"
+                    >
+                        <CloseIcon type="primary" />
+                    </button>
+
+                    {children}
+                </div>
+            </div>
+        </Portal>
+    ) : null;
+};
+
+export default Modal;
+
+// type Props = {
+//   title: string,
+//   onClose?: () => void,
+//   children: React.ReactNode | React.ReactNode[],
+// };
---
+++ src/components/OrderInfo/OrderDetails.jsx
@@ -0,0 +1,24 @@
+import doneImg from "../../img/done.png";
+import styles from "./OrderDetails.module.css";
+
+function OrderDetails({ item, count }) {
+    return (
+        <section className={styles.wrapper}>
+            <p className={`${styles.digits} text text_type_digits-large pt-30`}>
+                034536
+            </p>
+            <p className="text text_type_main-default pt-8">
+                идентификатор заказа
+            </p>
+            <img src={doneImg} alt="done" className="pt-15" />
+            <p className="text text_type_main-small pt-15">
+                Ваш заказ начали готовить
+            </p>
+            <p className="text text_type_main-default text_color_inactive pt-2 pb-30">
+                Дождитесь готовности на орбитальной станции
+            </p>
+        </section>
+    );
+}
+
+export default OrderDetails;
